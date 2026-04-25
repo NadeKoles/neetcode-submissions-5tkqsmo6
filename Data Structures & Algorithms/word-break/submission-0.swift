6df61d2ee70870ff81cacb63f31b4a5840dd27f8
NadeKoles <131354991+NadeKoles@users.noreply.github.com>
2026-04-25 21:59:19 +0300

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+        var dp = [Bool](repeating: false, count: s.count + 1)
+        dp[0] = true
+        
+        for i in 1...s.count {
+            for j in 0...i {
+                let start = s.index(s.startIndex, offsetBy: j)
+                let end = s.index(s.startIndex, offsetBy: i)
+                let word = String(s[start..<end])
+
+                if wordDict.contains(word) && dp[j] == true {
+                    dp[i] = true
+                    break
+                } 
+            }
+        }
+        return dp[s.count]
+    }
+}
